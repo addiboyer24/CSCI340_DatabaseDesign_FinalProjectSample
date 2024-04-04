194176152bcc6c83bce023ba674d9f24baaf683b
Addison Boyer <AddisonB@consumerdirectcare.com>
2024-04-03 18:52:38 -0600

    Adding initial base project

---
+++ final-project/src/test/java/database/design/finalproject/FinalProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package database.design.finalproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FinalProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
